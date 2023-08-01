da07a88732a1b2f2c2cd7b63d5fa453122dede9d
sanjay parajuli <sanjayparajuli343@gmail.com>
2023-08-01 18:20:35 +0545

    Just implemeted redis for images ranking system

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -33,9 +33,11 @@ INSTALLED_APPS = [
     'images',
     'easy_thumbnails',
     'actions',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -160,3 +162,11 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]   
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
